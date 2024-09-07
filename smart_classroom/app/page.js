6fe1b1a13418955364f0dee2415368948b1ce630
CodingPheonix <sybiswas115@gmail.com>
2024-09-07 19:53:30 +0530

    Basic Next App created

---
+++ smart_classroom/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+   <div className="grid place-items-center h-screen w-screen">
+    I HAVE STARTED THIS AND WILL FINISH THIS
+   </div>
+   </>
+  );
+}
